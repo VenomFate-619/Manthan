9fd659299cffabc5c5a25bbe8e8c5a3aa379aca4
Aavishkar <aavishkarmishra@gmail.com>
2021-06-06 08:58:20 +0530

    Added Check Scores Section

--- server.js
+++ server.js
@@ -25,11 +25,7 @@ if (process.env.NODE_ENV === 'production') {
     res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
   });
 }
-app.use(express.static('client/build'));
 
-app.get('*', (req, res) => {
-  res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
-});
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
